8f8aecd1ad9aafeba712c0ae24e86be33d6d91c7
Andreea <andelenasamur@gmail.com>
2023-03-30 12:39:26 +0200

    create fruits module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FruitsComponent } from './fruits/fruits.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FruitsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/fruits-list.ts
@@ -0,0 +1,14 @@
+import { Fruit } from './fruit';
+
+export const FRUITS: Fruit[] = [
+  { id: 1, name: 'Banana' },
+  { id: 2, name: 'Grapes' },
+  { id: 3, name: 'Apple' },
+  { id: 4, name: 'Orange' },
+  { id: 5, name: 'Coconut' },
+  { id: 6, name: 'Strawberry' },
+  { id: 7, name: 'Blackberry' },
+  { id: 8, name: 'Pineapple' },
+  { id: 9, name: 'Watermelon' },
+  { id: 10, name: 'Cherry' },
+];
